ee9e966decd42b0cbd92de3a2959d77d3052195c
Stuart McKeown <stuart.mckeown@xero.com>
2017-05-06 17:32:41 +1200

    Removing font awesome zip

---
+++ scripts/navigation.js
@@ -0,0 +1,6 @@
+// $(document).ready(function () {
+//     $(".navbar a").on("click", function(){
+//        $(".navbar").find(".active").removeClass("active");
+//        $(this).addClass("active");
+//     });
+// });
